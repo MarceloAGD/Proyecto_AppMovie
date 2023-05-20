0b02703a4b5eee649e8f671073e7296567b7d6f4
Diego Barros <diego.barros@alumnos.ucn.cl>
2023-05-20 03:10:33 -0400

    Implementacion de cors

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Configurar opciones de CORS
+  app.use(cors({
+    origin: 'http://localhost:3000',
+    credentials: true,
+  }));
+
   await app.listen(4000);
 }
+
 bootstrap();
